9a2636fad04e98c1e2e290dbab0c053ba45873e2
sirAndre1337 <andrelluis17@hotmail.com>
2018-12-04 16:47:07 -0200

    Estrutura do projeto e configuraçao do HibernateUtil

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.framework.implementacao.crud;
+
+public class VariavelConexaoUtil {
+	
+	/**
+	 * Nome do caminho do JNDI datasource TOMCAT
+	 */
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
